815273e4d72af61c48377e334b43b7b1b78b54f3
Ryann <rmahajan4@wisc.edu>
2022-12-11 15:31:05 -0600

    Added compose blank screen

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.photogallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import com.example.photogallery.ui.PhotoGrid
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            PhotoGrid()
+        }
     }
 }
---
+++ app/src/main/java/com/example/photogallery/ui/GalleryScreen.kt
@@ -0,0 +1,15 @@
+package com.example.photogallery.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun GalleryScreen() {
+    PhotoGrid()
+}
+
+@Composable
+fun PhotoGrid() {
+
+}
