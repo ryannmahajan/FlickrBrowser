5eb7a4e32631ba2e7bd7f4cae97badf099755a9d
Ryann <rmahajan4@wisc.edu>
2022-12-11 15:46:46 -0600

    Added lazyGrid showing static images

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.photogallery
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import com.example.photogallery.ui.PhotoGrid
+import com.example.photogallery.ui.GalleryScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhotoGrid()
+            GalleryScreen()
         }
     }
 }
--- app/src/main/java/com/example/photogallery/ui/GalleryScreen.kt
+++ app/src/main/java/com/example/photogallery/ui/GalleryScreen.kt
@@ -1,7 +1,16 @@
 package com.example.photogallery.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.photogallery.R
+
 
 @Preview
 @Composable
@@ -10,6 +19,17 @@ fun GalleryScreen() {
 }
 
 @Composable
-fun PhotoGrid() {
-
+private fun PhotoGrid() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+    ) {
+        items(count = 9) { index ->
+            Image(
+                painterResource(R.drawable.bill_up_close),
+                contentDescription= "",
+                contentScale = ContentScale.Crop,
+                modifier= Modifier.fillMaxSize()
+            )
+        }
+    }
 }
