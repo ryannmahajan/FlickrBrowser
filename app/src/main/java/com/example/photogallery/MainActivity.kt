750676abc76260f70b37d66d02d76b125ccb76c4
Ryann <rmahajan4@wisc.edu>
2023-01-14 16:58:07 -0500

    Added dummy SearchView

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -5,37 +5,42 @@ import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.MenuItemCompat
 import com.example.photogallery.ui.GalleryScreen
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel:PhotoGalleryViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GalleryScreen()
+            GalleryScreen(viewModel)
         }
 
     }
-//
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        menuInflater.inflate(R.menu.main_menu, menu)
-//
-//        val searchViewItem = menu.findItem(R.id.search_bar)
-//        val searchView = MenuItemCompat.getActionView(searchViewItem) as SearchView
-//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-//
-//            override fun onQueryTextSubmit(query: String): Boolean {
-//                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
-//                return true
-//            }
-//
-//            override fun onQueryTextChange(newText: String): Boolean {
-//                return false
-//            }
-//        })
-//        return super.onCreateOptionsMenu(menu)
-//    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        val searchViewItem = menu.findItem(R.id.search_bar)
+        val searchView = MenuItemCompat.getActionView(searchViewItem) as SearchView
+
+        searchView.setQuery(viewModel.searchQuery.value, false)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
 
 
 }
--- app/src/main/java/com/example/photogallery/ui/GalleryScreen.kt
+++ app/src/main/java/com/example/photogallery/ui/GalleryScreen.kt
@@ -1,18 +1,35 @@
+@file:OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
+
 package com.example.photogallery.ui
 
+import androidx.appcompat.widget.SearchView
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,6 +37,7 @@ import com.example.photogallery.PhotoGalleryViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.example.photogallery.GalleryItem
+import com.example.photogallery.R
 
 @Preview
 @Composable
@@ -27,58 +45,79 @@ fun GalleryScreen(viewModel: PhotoGalleryViewModel = viewModel()) {
     val galleryItemsState = viewModel.galleryItems.collectAsState()
     val queryState = viewModel.searchQuery.collectAsState()
 
-    Column {
-//        SearchView(queryState.value, onQueryChange = {
-//            // TODO: get pics from viewmodel
-//        })
-        PhotoGrid(galleryItemsState.value)
-    }
+    PhotoGrid(galleryItemsState.value)
+
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchView(query:String, onQueryChange:(String)->Unit) {
-    TextField(
-        value = query,
-        onValueChange = {v -> onQueryChange(v)},
-        modifier = Modifier.fillMaxWidth(),
-        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
-        leadingIcon = {
+fun SearchBar(
+    searchText: String,
+    placeholderText: String = "",
+    onSearchTextChanged: (String) -> Unit = {},
+    onClearClick: () -> Unit = {},
+    onNavigateBack: () -> Unit = {}
+) {
+    var showClearButton by remember { mutableStateOf(false) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
+
+    TopAppBar(title = { Text("") }, navigationIcon = {
+        IconButton(onClick = { onNavigateBack() }) {
             Icon(
-                Icons.Default.Search,
-                contentDescription = "",
-                modifier = Modifier
-                    .padding(15.dp)
-                    .size(24.dp)
+                imageVector = Icons.Filled.ArrowBack,
+                modifier = Modifier,
+                contentDescription = ""
             )
-        },
-        trailingIcon = {
-            if (query != "") {
-                IconButton(
-                    onClick = {
-                        TODO("What to do on clear")
-                    }
-                ) {
-                    Icon(
-                        Icons.Default.Close,
-                        contentDescription = "",
-                        modifier = Modifier
-                            .padding(15.dp)
-                            .size(24.dp)
-                    )
+        }
+    }, actions = {
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 2.dp)
+                .onFocusChanged { focusState ->
+                    showClearButton = (focusState.isFocused)
                 }
-            }
-        },
-        singleLine = true,
-        shape = RectangleShape, // The TextFiled has rounded corners top left and right by default
-        colors = TextFieldDefaults.textFieldColors(
-            textColor = Color.White,
-            cursorColor = Color.White,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
+                .focusRequester(focusRequester),
+            value = searchText,
+            onValueChange = onSearchTextChanged,
+            placeholder = {
+                Text(text = placeholderText)
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            trailingIcon = {
+                AnimatedVisibility(
+                    visible = showClearButton,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    IconButton(onClick = { onClearClick() }) {
+                        Icon(
+                            imageVector = Icons.Filled.Close,
+                            contentDescription = ""
+                        )
+                    }
+
+                }
+            },
+            maxLines = 1,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+                keyboardController?.hide()
+            }),
         )
-    )
+
+
+    })
+
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 }
 
 @Composable
