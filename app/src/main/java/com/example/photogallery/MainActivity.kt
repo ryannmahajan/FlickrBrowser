30916c1460a3479da25a8d1b58e8e7adf9e9f3a2
Ryann <rmahajan4@wisc.edu>
2023-01-14 14:19:27 -0500

    Removed unnecesary imports

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -2,14 +2,8 @@ package com.example.photogallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.activity.compose.setContent
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.example.photogallery.recyclerview.PhotoListAdapter
 import com.example.photogallery.ui.GalleryScreen
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
